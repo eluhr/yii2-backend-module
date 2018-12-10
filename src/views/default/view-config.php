da3d792ed917285f8348e52c195426ab36f633a9
Tobias Munk <schmunk@usrbin.de>
2018-12-10 07:31:00 +0100

    updated config view

--- src/views/default/view-config.php
+++ src/views/default/view-config.php
@@ -4,6 +4,7 @@ namespace _;
 
 use insolita\wgadminlte\Box;
 use yii\bootstrap\Tabs;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Json;
 use yii\helpers\VarDumper;
 use yii\widgets\ListView;
@@ -64,49 +65,40 @@ $this->title = "Configuration";
 <?php endforeach ?>
 <?php $this->endBlock('components') ?>
 
+
+
 <?php $this->beginBlock('modules') ?>
-<?php foreach ($modules as $name => $element): ?>
-    <?php Box::begin([
-        'title' => $name,
-        'collapse' => true,
-        'collapse_remember' => false,
-    ]) ?>
-
-    <div class="row">
-        <div class="col-sm-2">
-            <b><?= $name ?></b>
-        </div>
-        <div class="col-sm-10">
-<pre>
-<?= VarDumper::dumpAsString($element, 2, true) ?>
-</pre>
-        </div>
-    </div>
-
-    <?php Box::end() ?>
-<?php endforeach ?>
+<?php
+foreach ($modules AS $name => $module) {
+    $moduleItems[] = [
+        'label' => $name,
+        'content' => '<pre>' . VarDumper::dumpAsString($module, 2, true) . '</pre>'
+    ];
+}
+?>
+<?= Tabs::widget(['items' => $moduleItems]) ?>
 <?php $this->endBlock('modules') ?>
 
 
 <div class="nav-tabs-custom">
     <?= Tabs::widget([
-        'items' => [
-            [
-                'label' => 'Params',
-                'content' => $this->blocks['params'],
-            ],
-            [
-                'label' => 'Components',
-                'content' => $this->blocks['components'],
-            ],
-            [
-                'label' => 'Modules',
-                'content' => $this->blocks['modules'],
-            ],
-            [
-                'label' => 'Controllers',
-                'content' => $this->blocks['controllers'],
-            ],
-        ],
-    ]) ?>
+                         'items' => [
+                             [
+                                 'label' => 'Params',
+                                 'content' => $this->blocks['params'],
+                             ],
+                             [
+                                 'label' => 'Components',
+                                 'content' => $this->blocks['components'],
+                             ],
+                             [
+                                 'label' => 'Modules',
+                                 'content' => $this->blocks['modules'],
+                             ],
+                             [
+                                 'label' => 'Controllers',
+                                 'content' => $this->blocks['controllers'],
+                             ],
+                         ],
+                     ]) ?>
 </div>
